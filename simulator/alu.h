ea64649a74e6e9562154a90228fb898e85cb6a4c
crazyproger <crazyproger@gmail.com>
2026-01-24 11:57:52 +0300

    ALU implementation

---
+++ simulator/alu.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <assert.h>
+#include <stdint.h>
+
+enum ALUFunctionTypes {
+	AND = 0,
+	OR = 1,
+	INVB = 2,
+	SUM = 3
+};
+
+struct ALUFunction {
+	uint8_t func : 2;
+	uint8_t ena : 1;
+	uint8_t enb : 1;
+	uint8_t inva : 1;
+	uint8_t inc : 1;
+	uint8_t sll8 : 1;
+	uint8_t sra1 : 1;
+};
+
+class ALU {
+public:
+	ALU() {}
+	~ALU() {}
+
+	void SetInput(uint32_t a, uint32_t b) {
+		a_ = a;
+		b_ = b;
+	}
+
+	void SetFunction(ALUFunction f) {
+		f_ = f;
+	}
+
+	void Reset() {
+		SetInput(0, 0);
+		SetFunction({ 0 });
+		result_ = 0;
+		n_ = 0;
+		z_ = 0;
+	}
+
+	void Execute() {
+		uint32_t a = (f_.ena ? a_ : 0);
+		uint32_t b = (f_.enb ? b_ : 0);
+		if (f_.inva) {
+			a = ~a;
+		}
+		switch (f_.func) {
+		case ALUFunctionTypes::AND:
+			result_ = a & b;
+			break;
+		case ALUFunctionTypes::OR:
+			result_ = a | b;
+			break;
+		case ALUFunctionTypes::INVB:
+			result_ = ~b;
+			break;
+		case ALUFunctionTypes::SUM:
+			result_ = a + b;
+			break;
+		default:
+			assert(0);
+		}
+		if (f_.inc) {
+			result_ += 1;
+		}
+		if (f_.sll8) {
+			result_ <<= 8;
+		}
+		uint32_t msb = (result_ >> 31) & 1;
+		if (f_.sra1) {
+			result_ >>= 1;
+			result_ |= (msb << 31);
+		}
+		if (result_ == 0) {
+			z_ = 1;
+		}
+		if (msb == 1) {
+			n_ = 1;
+		}
+	}
+
+	uint32_t GetResult() {
+		return result_;
+	}
+
+	uint8_t GetN() {
+		return n_;
+	}
+
+	uint8_t GetZ() {
+		return z_;
+	}
+
+private:
+	uint32_t a_ = 0;
+	uint32_t b_ = 0;
+	ALUFunction f_{ 0 };
+	
+	uint32_t result_ = 0;
+	uint8_t n_ = 0;
+	uint8_t z_ = 0;
+};
---
+++ tests/test_alu.cpp
@@ -0,0 +1,151 @@
+#include "doctest.h"
+
+#include "alu.h"
+
+class ALUTestObject {
+public:
+	static ALU& Create() {
+		static ALU alu;
+		alu.Reset();
+		return alu;
+	}
+};
+
+TEST_CASE("ALUFunction Size") {
+	CHECK(sizeof(ALUFunction) == 1);
+}
+
+TEST_CASE("ALUFunction AND") {
+	auto& alu = ALUTestObject::Create();
+	alu.SetInput(4, 5);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::AND, 1, 1, 0, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 4);
+}
+
+TEST_CASE("ALUFunction OR") {
+	auto& alu = ALUTestObject::Create();
+	alu.SetInput(4, 5);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::OR, 1, 0, 0, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 4);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::OR, 0, 1, 0, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 5);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::OR, 1, 0, 1, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == ~4);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::OR, 1, 1, 0, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 5);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::OR, 0, 0, 0, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 0);
+}
+
+TEST_CASE("ALUFunction INVB") {
+	auto& alu = ALUTestObject::Create();
+	alu.SetInput(4, 5);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::INVB, 0, 1, 0, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == ~5);
+}
+
+TEST_CASE("ALUFunction SUM") {
+	auto& alu = ALUTestObject::Create();
+	alu.SetInput(4, 5);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 1, 0, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 9);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 1, 0, 1, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 10);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 0, 0, 1, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 5);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 0, 1, 0, 1, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 6);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 1, 1, 1, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 1);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 0, 1, 1, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 4);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 0, 1, 1, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == -4);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 0, 0, 0, 1, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 1);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 0, 0, 1, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == -1);
+}
+
+TEST_CASE("ALUFunction SLL8") {
+	auto& alu = ALUTestObject::Create();
+	alu.SetInput(4, 5);
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 1, 0, 0, 1, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == (9 << 8));
+}
+
+TEST_CASE("ALUFunction SRA1") {
+	auto& alu = ALUTestObject::Create();
+	alu.SetInput(4, 5);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 1, 0, 0, 0, 1 });
+	alu.Execute();
+	CHECK(alu.GetResult() == (9 >> 1));
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 0, 1, 1, 0, 1 });
+	alu.Execute();
+	CHECK(alu.GetResult() == -2);
+}
+
+TEST_CASE("ALUFunction Z") {
+	auto& alu = ALUTestObject::Create();
+
+	alu.SetInput(4, 4);
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 1, 0, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 8);
+	CHECK(alu.GetZ() == 0);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 1, 1, 1, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 0);
+	CHECK(alu.GetZ() == 1);
+}
+
+TEST_CASE("ALUFunction N") {
+	auto& alu = ALUTestObject::Create();
+
+	alu.SetInput(5, 4);
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 1, 0, 0, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == 9);
+	CHECK(alu.GetN() == 0);
+
+	alu.SetFunction(ALUFunction{ ALUFunctionTypes::SUM, 1, 1, 1, 1, 0, 0 });
+	alu.Execute();
+	CHECK(alu.GetResult() == -1);
+	CHECK(alu.GetN() == 1);
+}
